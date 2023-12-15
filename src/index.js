5d882d2fcbd7b3876236e6f6151ca3f71392f01a
Izzeldeen Ashour <ezraashour00@gmail.com>
2023-12-15 08:14:22 +0300

    push

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import React from "react";
-
-import ReactDOM from "react-dom/client";
-// import ReactDOM from 'react-dom';
-
-import App from "./App";
-import "./index.css";
-
-// REACT 18
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+ import StarRating from './starRating';
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} messages={['Terrible','bad','ok','good','amazing']} defaultRating={3}/>
+    <StarRating size={24} color="red" className="test"/>
   </React.StrictMode>
 );
 
-// REACT 17
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
